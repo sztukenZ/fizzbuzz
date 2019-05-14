3e71acd33f2578b4754d8dbf4ef8e8dbb536a974
Chris <50459148+sztukenZ@users.noreply.github.com>
2019-05-14 18:46:14 +0200

    fizzbuzz use case added when divideable by 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz():
     for i in range(1,101):
+        if not i % 3 and not i % 5:
+            print('fizzbuzz')
         if not i % 3:
             print('fizz')
         elif not i % 5:
