f97c3eb131535a7a72d438d109f1a0040ba8ed77
sztukenZ <sztuku@gmail.com>
2019-05-14 20:01:04 +0200

    Added limeric

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,15 @@
 def twofer(who='you'):
     return f'One for {who}, one for me'
 
+def limerick():
+  lim_list = ['Był skrzypek rodem z Prabutów,',
+              'miał nogi za duże do butów.',
+              'Wszystkie go uwierały,',
+              'więc nosił futerały',
+              'od skrzypiec zamiast butów.']
+  for line in lim_list:
+      print(line)
+
 def fizzbuzz():
     for i in range(1,101):
         if not i % 3 and not i % 5:
@@ -16,7 +25,7 @@ def fizzbuzz():
 
 if __name__ == '__main__':
     while True:
-        fizzbuzz()
+        limerick()
         shall_continue = input('Print again?')
         if shall_continue.lower() != 'y':
             break
